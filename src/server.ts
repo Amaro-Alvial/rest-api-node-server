54a50270911110fae002ad46fb812f95666591a3
Amaro-Alvial-Student <a.alvialbravo@uandresbello.edu>
2025-05-18 13:56:20 -0400

    Se realiza la conexión con la base de datos. Se define el router.ts, se ajusta el tsconfig para funcionar con sequelize-typescript. Finalmente se define un primer modelo en la carpeta Models, siendo este "Product.ts"

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,25 @@
 import express from 'express'
+import colors from 'colors'
+import router from './router'
+import db from './config/db'
+
+// Conectar a la base de datos
+async function conectDB(){
+        try {
+                await db.authenticate()
+                db.sync()
+                console.log(colors.magenta.bold('Conexión existosa a la base de datos'))
+
+        } catch (error) {
+                console.log(colors.bold.red('Hubo un error al conectar a la base de datos'))
+                console.warn(colors.yellow(error))
+        }
+}
+
+conectDB()
 
 const server = express()
 
-
-// Routing
-server.get('/', (req, res) => {
-        res.json('Desde Get')
-})
-server.post('/', (req, res) => {
-        res.json('Desde Post')
-})
-server.put('/', (req, res) => {
-        res.json('Desde Put')
-})
-server.patch('/', (req, res) => {
-        res.json('Desde Patch')
-})
-server.delete('/', (req, res) => {
-        res.json('Desde Delete')
-})
-
+server.use('/api/products', router)
 
 export default server
