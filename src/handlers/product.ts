ee10c727cc5a0684bc3465d8644064ba71ecc7ae
Amaro-Alvial-Student <a.alvialbravo@uandresbello.edu>
2026-06-15 22:25:09 -0400

    Se termina el CRUD para productos en este Rest-API

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,6 +2,35 @@ import { Request, Response } from "express"
 import Product from "../models/Product"
 import colors from 'colors'
 
+export const getProducts = async (req : Request, res : Response) => {
+    try {
+        const products = await Product.findAll({
+            order: [
+                ['id', 'ASC']
+            ]
+        })
+        res.json({data: products})
+    } catch (error) {
+        console.log(colors.bgRed(error))
+    }
+}
+
+export const getProductsById = async (req : Request, res : Response) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+        if(!product){
+            return res.status(404).json({
+                error: 'Producto no encontrado'
+            })
+        }
+
+        res.json({data: product})
+    } catch (error) {
+        console.log(colors.bgRed(error))
+    }
+}
+
 export const createProduct = async (req : Request, res : Response) => {
     try {  
         const product = await Product.create(req.body)
@@ -9,4 +38,55 @@ export const createProduct = async (req : Request, res : Response) => {
     } catch (error) {
         console.log(colors.bgRed(error))
     }
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+    // Verificar si el producto existe
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if(!product){
+        return res.status(404).json({
+            error: 'Producto no encontrado'
+        })
+    }
+
+    // Actualizar
+
+    await product.update(req.body)
+    await product.save()
+
+    res.json({data: product})
+}
+
+export const updateAvailability = async (req: Request, res: Response) => {
+    // Verificar si el producto existe
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if(!product){
+        return res.status(404).json({
+            error: 'Producto no encontrado'
+        })
+    }
+
+    // Actualizar
+    product.availability = !product.dataValues.availability
+    await product.save()
+    res.json({data: product})
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+        // Verificar si el producto existe
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if(!product){
+        return res.status(404).json({
+            error: 'Producto no encontrado'
+        })
+    }
+
+    await product.destroy()
+    res.json({data: 'Producto eliminado'})
 }
--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,18 @@
 import { Router } from "express"
-import { createProduct } from "./handlers/product"
-import { body } from "express-validator"
+import { createProduct, deleteProduct, getProducts, getProductsById, updateAvailability, updateProduct } from "./handlers/product"
+import { body, param } from "express-validator"
 import { handleInputErrors } from "./middleware"
 
 const router = Router()
 
 // Routing
-router.get('/', (req, res) => {
-    res.json('Desde Get')
-})
+router.get('/', getProducts)
+router.get('/:id', 
+    param('id').isInt().withMessage('ID no válido'),
+    handleInputErrors,
+    getProductsById
+)
+
 router.post('/', 
     // Validación
     body('name')
@@ -17,19 +21,35 @@ router.post('/',
         .isNumeric().withMessage('El valor debe ser númerico')
         .notEmpty().withMessage('El precio no puede estar vacio')
         .custom((value) => value > 0).withMessage('El precio debe ser mayor a 0'),
-        handleInputErrors,
+    handleInputErrors,
     createProduct
 )
 
-router.put('/', (req, res) => {
-   res.json('Desde Put')
-})
-router.patch('/', (req, res) => {
-    res.json('Desde Patch')
-})
-router.delete('/', (req, res) => {
-    res.json('Desde Delete')
-})
+router.put('/:id',
+    param('id').isInt().withMessage('ID no válido'),
+    body('name')
+        .notEmpty().withMessage('El nombre de Producto no puede ir vacio'),
+    body('price')
+        .isNumeric().withMessage('El valor debe ser númerico')
+        .notEmpty().withMessage('El precio no puede estar vacio')
+        .custom((value) => value > 0).withMessage('El precio debe ser mayor a 0'),
+    body('availability')
+        .isBoolean().withMessage('Valor para disponibilidad no válido'),
+    handleInputErrors,
+    updateProduct
+)
+
+router.patch('/:id', 
+    param('id').isInt().withMessage('ID no válido'),
+    handleInputErrors,
+    updateAvailability
+)
+
+router.delete('/:id', 
+    param('id').isInt().withMessage('ID no válido'),
+    handleInputErrors,
+    deleteProduct
+)
 
 export default router
 
