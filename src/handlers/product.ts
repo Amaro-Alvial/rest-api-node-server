679c8b48171236cb6a14f67396cf42a966e45689
Amaro-Alvial-Student <a.alvialbravo@uandresbello.edu>
2026-07-08 17:03:04 -0400

    Se realiza el testeo de la api rest con jest y supertest. Habiendo probado casi todo el código, con la excepción de 3 catch de try/catch. Logrando un 93% de cobertura de código.

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -34,7 +34,7 @@ export const getProductsById = async (req : Request, res : Response) => {
 export const createProduct = async (req : Request, res : Response) => {
     try {  
         const product = await Product.create(req.body)
-        res.json({data: product})
+        res.status(201).json({data: product})
     } catch (error) {
         console.log(colors.bgRed(error))
     }
@@ -71,7 +71,7 @@ export const updateAvailability = async (req: Request, res: Response) => {
     }
 
     // Actualizar
-    product.availability = !product.dataValues.availability
+    product.setDataValue('availability', !product.getDataValue('availability'))
     await product.save()
     res.json({data: product})
 }
--- src/server.ts
+++ src/server.ts
@@ -5,19 +5,18 @@ import db from './config/db'
 import Product from './models/Product'
 
 // Conectar a la base de datos
-async function conectDB(){
+export async function connectDB(){
     try {
             await db.authenticate()
             db.sync({alter: true})
             db.addModels([Product])
-            console.log(colors.magenta.bold('Conexión existosa a la base de datos'))
+            // console.log(colors.magenta.bold('Conexión existosa a la base de datos'))
     } catch (error) {
         console.log(colors.bold.red('Hubo un error al conectar a la base de datos'))
         console.warn(colors.yellow(error))
     }        
 }
-
-conectDB()
+connectDB()
 
 //Instancia de express
 const server = express()
@@ -26,5 +25,9 @@ const server = express()
 server.use(express.json())
 
 server.use('/api/products', router)
+
+server.get('/api', (req, res) => {
+    res.json({msg: 'Desde API'})
+})
 
 export default server
